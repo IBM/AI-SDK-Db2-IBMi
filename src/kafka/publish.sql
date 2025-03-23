ed723e519f75d3b503a38fd1f30e83bbe15f4606
Jesse Gorzinski <17914061+ThePrez@users.noreply.github.com>
2025-03-23 17:42:16 -0500

    implement Kafka publish

--- src/kafka/publish.sql
+++ src/kafka/publish.sql
@@ -1,25 +1,41 @@
 
--- WIP
 create or replace function watsonx.kafka_publish(msgdata varchar(32000) ccsid 1208 default NULL, topic varchar(1000) ccsid 1208 default NULL, key varchar(1000) ccsid 1208 default NULL)
-  RETURNS TABLE(resp CLOB(2G) CCSID 1208, hdrs CLOB(2G) CCSID 1208 )
+  RETURNS varchar(32000) ccsid 1208
   modifies sql data
   not deterministic
   no external action
   set option usrprf = *user, dynusrprf = *user, commit = *none
 begin
   declare fullUrl varchar(32500) ccsid 1208 default NULL;
-  declare apiresult  CLOB(2G) CCSID 1208 default NULL;
+  declare apierr  varchar(32500) CCSID 1208 default NULL;
+  declare response_header Varchar(10000) CCSID 1208;
+  declare response_message Varchar(10000) CCSID 1208;
+  declare response_code int default 500;
+  
   declare http_options varchar(32400) ccsid 1208 default '{"header":"Content-Type,application/vnd.kafka.json.v2+json" ,
-                         "header":"Accept,application/vnd.kafka.v2+json" 
-                         }';
+                                                           "header":"Accept,application/vnd.kafka.v2+json" 
+                                                          }';
   set fullUrl = watsonx.kafka_getprotocol() concat '://' concat watsonx.kafka_getbroker() concat ':' concat watsonx.kafka_getport() concat '/topics/' concat topic;
-
-  return select * from table(QSYS2.HTTP_POST_VERBOSE(
+  
+  select RESPONSE_MESSAGE, RESPONSE_HTTP_HEADER
+  into response_message, response_header
+  from table(QSYS2.HTTP_POST_VERBOSE(
                         fullUrl,
                         json_object('records': json_array(json_object('key': key, 'value':msgdata))),
                         http_options));
---   set rest_result = QSYS2.HTTP_POST(
---                         fullUrl,
---                         json_object('records': json_array(json_object('key': key, 'value':msgdata))),
---                         http_options));
+  set response_code = json_value(response_header, '$.HTTP_STATUS_CODE');
+  set apierr = json_value(response_message, '$.offsets[0].error');
+  
+  if(apierr is not null) then
+    call systools.lprintf('Kafka publish returned error: ' concat apierr);
+  else 
+    set apierr = 'An unknown error has occurred';
+  end if;
+  
+  if (response_code = 200) then
+    return null;
+  end if;
+
+  signal sqlstate '38002' set message_text = 'Add error has occured. Check the job log.';
+  return apierr;
 end;
