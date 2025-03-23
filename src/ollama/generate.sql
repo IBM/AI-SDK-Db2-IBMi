48b612a9de5d01290405ed65d1b48e1c124d4066
Jesse Gorzinski <17914061+ThePrez@users.noreply.github.com>
2025-03-23 18:03:01 -0500

    rewrite ollama generate function

--- src/ollama/generate.sql
+++ src/ollama/generate.sql
@@ -1,21 +1,33 @@
 create or replace function watsonx.ollama_generate(prompt varchar(1000) ccsid 1208, model_id varchar(1000) ccsid 1208 default NULL) 
-  RETURNS TABLE(output VARCHAR(32000) ccsid 1208 )
+  RETURNS clob(2G) ccsid 1208
   modifies sql data
   not deterministic
   no external action
   set option usrprf = *user, dynusrprf = *user, commit = *none
 begin
   declare fullUrl varchar(32500) ccsid 1208 default NULL;
+  declare apierr  varchar(32500) CCSID 1208 default NULL;
+  declare response_header Varchar(10000) CCSID 1208;
+  declare response_message Varchar(10000) CCSID 1208;
+  declare response_code int default 500;
+  
   declare http_options varchar(32400) ccsid 1208 default '{"ioTimeout":2000000}';
   set fullUrl = watsonx.ollama_getprotocol() concat '://' concat watsonx.ollama_getserver() concat ':' concat watsonx.ollama_getport() concat '/api/generate';
   
-  return SELECT ELEMENT as response
-    FROM TABLE (
-            SYSTOOLS.SPLIT(
-                    JSON_VALUE(QSYS2.HTTP_POST(
+  select RESPONSE_MESSAGE, RESPONSE_HTTP_HEADER
+  into response_message, response_header
+  from table(QSYS2.HTTP_POST_VERBOSE(
                         fullUrl,
                         json_object('model': watsonx.ollama_getmodel(), 'prompt': prompt, 'stream': false),
-                        http_options), 
-                    '$.response'),'
-'));
+                        http_options));
+  call systools.lprintf('r: ' concat response_message);
+  set response_code = json_value(response_header, '$.HTTP_STATUS_CODE');
+  
+  if (response_code != 200) then
+    call systools.lprintf('Ollama generation request returned error: ' concat response_message);
+    signal sqlstate '38002' set message_text = 'Add error has occured. Check the job log.';
+    return null;
+  end if;
+
+  return JSON_VALUE(response_message,'$.response' returning CLOB(2G) ccsid 1208);
 end;
