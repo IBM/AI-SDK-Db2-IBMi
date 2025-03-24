4292cc82d4ac0194d6e82c1cc8b5d110dab09da1
Jesse Gorzinski <17914061+ThePrez@users.noreply.github.com>
2025-03-23 19:12:45 -0500

    Add Twilio

--- src/conf.sql
+++ src/conf.sql
@@ -21,8 +21,13 @@ create or replace variable watsonx.kafka_port     INT                      defau
 create or replace variable watsonx.kafka_topic    varchar(1000) ccsid 1208 default NULL;
 
 -- variables for Slack
-create or replace variable watsonx.slack_clientsecret    varchar(1000) ccsid 1208 default NULL;
-create or replace variable watsonx.slack_clientid    varchar(1000) ccsid 1208 default NULL;
+create or replace variable watsonx.slack_clientsecret  varchar(1000) ccsid 1208 default NULL;
+create or replace variable watsonx.slack_clientid      varchar(1000) ccsid 1208 default NULL;
+
+-- variables for 
+create or replace variable watsonx.twilio_number       varchar(1000) ccsid 1208 default NULL;
+create or replace variable watsonx.twilio_sid          varchar(1000) ccsid 1208 default NULL;
+create or replace variable watsonx.twilio_authtoken    varchar(1000) ccsid 1208 default NULL;
 
 create or replace table watsonx.conf
       (
@@ -43,6 +48,9 @@ create or replace table watsonx.conf
           kafka_topic varchar(1000) ccsid 1208 default NULL,
           slack_clientsecret varchar(1000) ccsid 1208 default NULL,
           slack_clientid varchar(1000) ccsid 1208 default NULL,
+          twilio_number varchar(1000) ccsid 1208 default NULL,
+          twilio_sid varchar(1000) ccsid 1208 default NULL,
+          twilio_authtoken varchar(1000) ccsid 1208 default NULL,
         PRIMARY KEY(USRPRF)
       )
       on replace preserve rows;
---
+++ src/twilio/utils.sql
@@ -0,0 +1,109 @@
+
+
+create or replace function watsonx.twilio_getnumber(phnum varchar(1000) ccsid 1208 default NULL) 
+  returns varchar(1000) ccsid 1208
+  modifies sql data
+begin
+  declare returnval varchar(1000) ccsid 1208;
+  call watsonx.conf_initialize();
+  set returnval = phnum;
+  if (returnval is not null) then return returnval;end if;
+  set returnval = watsonx.twilio_number;
+  if (returnval is not null) then return returnval;end if;
+  set returnval = (select twilio_number from watsonx.conf where USRPRF = CURRENT_USER);
+  if (returnval is not null) then return returnval;end if;
+  set returnval = (select twilio_number from watsonx.conf where USRPRF = '*DEFAULT');
+  return returnval;
+end;
+
+create or replace procedure watsonx.twilio_setnumberforjob(phnum varchar(1000) ccsid 1208 default NULL) 
+  modifies SQL DATA
+begin
+  set watsonx.twilio_number= phnum;
+end;
+create or replace procedure watsonx.twilio_setnumberforme(phnum varchar(1000) ccsid 1208 default NULL) 
+  MODIFIES SQL DATA
+begin
+  MERGE INTO watsonx.conf tt USING (
+    SELECT CURRENT_USER AS usrprf, phnum AS twilio_number
+      FROM sysibm.sysdummy1
+  ) live
+  ON tt.usrprf = live.usrprf
+  WHEN NOT MATCHED THEN INSERT (usrprf, twilio_number) VALUES (live.usrprf,
+      live.twilio_number)
+  WHEN MATCHED THEN UPDATE SET (usrprf, twilio_number) = (live.usrprf, live.twilio_number);
+end;
+
+create or replace function watsonx.twilio_getsid(account_sid varchar(1000) ccsid 1208 default NULL) 
+  returns varchar(1000) ccsid 1208
+  modifies sql data
+begin
+  declare returnval varchar(1000) ccsid 1208;
+  call watsonx.conf_initialize();
+  set returnval = account_sid;
+  if (returnval is not null) then return returnval;end if;
+  set returnval = watsonx.twilio_sid;
+  if (returnval is not null) then return returnval;end if;
+  set returnval = (select twilio_sid from watsonx.conf where USRPRF = CURRENT_USER);
+  if (returnval is not null) then return returnval;end if;
+  set returnval = (select twilio_sid from watsonx.conf where USRPRF = '*DEFAULT');
+  return returnval;
+end;
+
+create or replace procedure watsonx.twilio_setsidforjob(account_sid varchar(1000) ccsid 1208 default NULL) 
+  modifies SQL DATA
+begin
+  set watsonx.twilio_sid= account_sid;
+end;
+create or replace procedure watsonx.twilio_setsidforme(account_sid varchar(1000) ccsid 1208 default NULL) 
+  MODIFIES SQL DATA
+begin
+  MERGE INTO watsonx.conf tt USING (
+    SELECT CURRENT_USER AS usrprf, account_sid AS twilio_sid
+      FROM sysibm.sysdummy1
+  ) live
+  ON tt.usrprf = live.usrprf
+  WHEN NOT MATCHED THEN INSERT (usrprf, twilio_sid) VALUES (live.usrprf,
+      live.twilio_sid)
+  WHEN MATCHED THEN UPDATE SET (usrprf, twilio_sid) = (live.usrprf, live.twilio_sid);
+end;
+
+
+
+
+
+
+
+create or replace function watsonx.twilio_getauthtoken(authtoken varchar(1000) ccsid 1208 default NULL) 
+  returns varchar(1000) ccsid 1208
+  modifies sql data
+begin
+  declare returnval varchar(1000) ccsid 1208;
+  call watsonx.conf_initialize();
+  set returnval = authtoken;
+  if (returnval is not null) then return returnval;end if;
+  set returnval = watsonx.twilio_authtoken;
+  if (returnval is not null) then return returnval;end if;
+  set returnval = (select twilio_authtoken from watsonx.conf where USRPRF = CURRENT_USER);
+  if (returnval is not null) then return returnval;end if;
+  set returnval = (select twilio_authtoken from watsonx.conf where USRPRF = '*DEFAULT');
+  return returnval;
+end;
+
+create or replace procedure watsonx.twilio_setauthtokenforjob(authtoken varchar(1000) ccsid 1208 default NULL) 
+  modifies SQL DATA
+begin
+  set watsonx.twilio_authtoken= authtoken;
+end;
+create or replace procedure watsonx.twilio_setauthtokenforme(authtoken varchar(1000) ccsid 1208 default NULL) 
+  MODIFIES SQL DATA
+begin
+  MERGE INTO watsonx.conf tt USING (
+    SELECT CURRENT_USER AS usrprf, authtoken AS twilio_authtoken
+      FROM sysibm.sysdummy1
+  ) live
+  ON tt.usrprf = live.usrprf
+  WHEN NOT MATCHED THEN INSERT (usrprf, twilio_authtoken) VALUES (live.usrprf,
+      live.twilio_authtoken)
+  WHEN MATCHED THEN UPDATE SET (usrprf, twilio_authtoken) = (live.usrprf, live.twilio_authtoken);
+end;
